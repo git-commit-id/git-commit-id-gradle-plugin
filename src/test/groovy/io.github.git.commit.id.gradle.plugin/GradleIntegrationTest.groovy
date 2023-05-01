9e7b0aa954b3d813325cd1974175ab05358189d4
TheSnoozer <s.lange@alumni.tu-berlin.de>
2023-05-01 09:21:08 +0200

    add a dedicated test that ensures that basic task caching is working

--- src/test/groovy/io.github.git.commit.id.gradle.plugin/GradleIntegrationTest.groovy
+++ src/test/groovy/io.github.git.commit.id.gradle.plugin/GradleIntegrationTest.groovy
@@ -81,4 +81,61 @@ class GradleIntegrationTest extends AbstractGradleTest {
                 result.output
         )
     }
+
+    @Test
+    void cachingShouldWork() {
+        given: "a dummy project"
+        def projectDir = temporaryFolder
+
+        and: "caching is enabled"
+        // https://docs.gradle.org/current/userguide/build_cache.html#sec:build_cache_enable
+        new File(projectDir, "gradle.properties") << "org.gradle.caching=true"
+
+        and: "we produce a git properties file"
+        new File(projectDir, "build.gradle").withWriterAppend("UTF-8") {
+            it.write(
+                    """
+                    ${GitCommitIdPlugin.GIT_COMMIT_ID_EXTENSION_NAME} {
+                        generateGitPropertiesFile.set(true)
+                    }
+                    """.stripIndent()
+            )
+        }
+
+        when: "running the plugin"
+        def runner = GradleRunner.create()
+                .withPluginClasspath()
+                .withArguments(":${GitCommitIdPlugin.GIT_COMMIT_ID_TASK_NAME}", "--stacktrace", "--debug")
+                .withProjectDir(projectDir)
+
+        then: "the execution should be successfull"
+        def result = runner.build()
+        Assertions.assertEquals(
+                TaskOutcome.SUCCESS,
+                result.task(":${GitCommitIdPlugin.GIT_COMMIT_ID_TASK_NAME}")?.outcome,
+                result.output
+        )
+
+        and: "output exists"
+        def expectedGenerated = projectDir.toPath().resolve("build/git.properties").toFile()
+        Assertions.assertTrue(expectedGenerated.exists(), "Does not exists $expectedGenerated")
+        def originalLines = expectedGenerated.readLines()
+
+        and: "we delete the file by accident"
+        expectedGenerated.delete()
+        Assertions.assertFalse(expectedGenerated.exists(), "Should not exists $expectedGenerated")
+
+        when: "the plugin get's executed again"
+        result = runner.build()
+        Assertions.assertEquals(
+                TaskOutcome.FROM_CACHE,
+                result.task(":${GitCommitIdPlugin.GIT_COMMIT_ID_TASK_NAME}")?.outcome,
+                result.output
+        )
+        Assertions.assertTrue(expectedGenerated.exists(), "Was not regenerated $expectedGenerated")
+
+        and:
+        def newlyGeneratedLines = expectedGenerated.readLines()
+        Assertions.assertEquals(originalLines, newlyGeneratedLines)
+    }
 }
